3d87074c8bb48a5cc9e00bdb044d52331a51c126
Andreas Geiger <geiger911@googlemail.com>
2017-02-25 22:25:34 +0100

    Add solution file for the final exercise "Collatz-Folge" in lecture

---
+++ Lecture 3/Project.py
@@ -0,0 +1,24 @@
+#Define method which takes number and returns depending if even or odd a return value
+def collatz(number):
+    calculatedNumber = 0
+
+    if number%2 == 0:
+        calculatedNumber = int(number/2)
+    else:
+        calculatedNumber = number * 3 + 1
+
+    print(calculatedNumber)    
+    return calculatedNumber
+
+#User input
+
+try:
+    print("Input as number:")
+    
+    userInput = int(input())
+
+    while userInput!= 1:
+        userInput = collatz(userInput)
+
+except ValueError:
+    print("Please type in a number")
